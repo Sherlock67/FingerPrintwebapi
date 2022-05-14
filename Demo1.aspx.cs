eb485131bf6be6a34a4b30c835ce523c487cddab
Sherlock67 <alammushfiqul366@gmail.com>
2022-05-14 22:49:27 +0600

    locally verifying

---
+++ Demo1.aspx.cs
@@ -0,0 +1,18 @@
+﻿using NLog.Internal;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+
+
+public partial class Demo1 : System.Web.UI.Page
+{
+    SqlConnection myConnection = new SqlConnection(System.Configuration.ConfigurationManager.ConnectionStrings["DbConnection"].ConnectionString);
+
+    protected void Page_Load(object sender, EventArgs e)
+    {
+            
+    }
+
+}
